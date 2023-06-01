3e68bbe5668e1a6fbefce8c944b5e32e199efff1
ykay-q4 <yama.karimi08@gmail.com>
2023-06-01 02:20:17 -0400

    updated site url's

--- src/projectInfo.ts
+++ src/projectInfo.ts
@@ -1,14 +1,15 @@
 import placeholder from "./assets/placeholder-stock.png";
 import gamehub from "./assets/game-hub-hero.png";
+import gamehubcropped from "./assets/game-hub-hero-crop.png";
 import tracker from "./assets/expense-hero.png";
 const projectsInfo = [
   {
     id: 1,
-    img: gamehub,
+    img: gamehubcropped,
     title: "Game Hub",
     techBio: "Technologies Used: React,CSS",
-    github: "https://github.com/ykay220",
-    live: "https://www.linkedin.com/in/yama-karimi-6b1a6b213/",
+    github: "https://github.com/ykay220/Game-hub",
+    live: "https://game-hub.yamakarimi.com/",
     details: [
       { info: "Completely built with hooks and functional components" },
       { info: "Information on games pulled from a video game API" },
@@ -22,8 +23,8 @@ const projectsInfo = [
     id: 2,
     img: tracker,
     title: "Employee Tracking System",
-    github: "https://github.com/ykay220",
-    live: "https://www.linkedin.com/in/yama-karimi-6b1a6b213/",
+    github: "https://github.com/ykay220/Employee-Directory-and-Tracking-System",
+    live: "https://employee-tracker.yamakarimi.com/",
     techBio: "Technologies Used: React,CSS,Bootstrap",
     details: [
       { info: "Utilizing react-hook-form for form handling and validation" },
