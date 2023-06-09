ddcf33da91558931e86b63398ca71042d057b8fc
ykay-q4 <yama.karimi08@gmail.com>
2023-06-08 21:01:42 -0400

    style

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
-
+import theme from "./theme";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <CSSReset />
       <App />
     </ChakraProvider>
