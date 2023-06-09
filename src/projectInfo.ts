3cf655b4e78b674a366ae932426b78efdf7d11e9
ykay-q4 <yama.karimi08@gmail.com>
2023-06-08 20:15:30 -0400

    New Project added

--- src/projectInfo.ts
+++ src/projectInfo.ts
@@ -2,6 +2,7 @@ import placeholder from "./assets/placeholder-stock.png";
 import gamehub from "./assets/game-hub-hero.png";
 import gamehubcropped from "./assets/game-hub-hero-crop.png";
 import tracker from "./assets/expense-hero.png";
+import weather from "./assets/weather-screen.png";
 const projectsInfo = [
   {
     id: 1,
@@ -33,6 +34,25 @@ const projectsInfo = [
       },
       {
         info: "Implemented custom hooks",
+      },
+    ],
+  },
+  {
+    id: 3,
+    img: weather,
+    title: "Real Time 7 Day Weather Forecast",
+    github: "https://github.com/ykay220/Weatherapp-5-day-forecast",
+    live: "https://employee-tracker.yamakarimi.com/",
+    techBio: "Technologies Used: React,CSS",
+    details: [
+      {
+        info: "The weather app provides a user-friendly interface that allows users to easily input any city name and instantly retrieve the 7-day weather forecast for that location.",
+      },
+      {
+        info: "Leverages the useState and useEffect hooks in React, the app dynamically fetches weather data from a reliable weather forecast API. This ensures that users have access to up-to-date and accurate weather information.",
+      },
+      {
+        info: "The app presents the weather forecast for each day in a visually appealing manner, using cards to display detailed information such as temperature, humidity, wind speed, and weather conditions (e.g., sunny, cloudy, rainy).",
       },
     ],
   },
